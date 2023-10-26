f9554bb3c2da572315f3c0e8ba9c1c41055426b5
vamsi <vamsisunny055@gmail.com>
2023-10-26 10:07:57 -0500

    commit for 4th time

--- insurance-management-ecosystem/src/test/java/com/insurance/AppTest.java
+++ insurance-management-ecosystem/src/test/java/com/insurance/AppTest.java
@@ -1,20 +1,20 @@
 package com.insurance;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * Unit test for simple App.
- */
+
+@SpringBootTest
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	void contextLoads() {
+         int result = 2 + 2;
+        assertEquals(4, result);
+	}
+
 }
